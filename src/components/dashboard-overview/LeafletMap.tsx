12da2c3eaa81b5c8877d3e8e4fa4a20bdd1e6c69
pedromartins11 <a23527@alunos.ipca.pt>
2025-04-18 12:13:16 +0100

    Color map updated

--- src/components/dashboard-overview/LeafletMap.tsx
+++ src/components/dashboard-overview/LeafletMap.tsx
@@ -1,7 +1,5 @@
 import { MapContainer, GeoJSON } from 'react-leaflet';
 import { useEffect, useState } from 'react';
-import 'leaflet/dist/leaflet.css';
-import L from 'leaflet';
 
 export default function Leafletmap() {
   const [geoData, setGeoData] = useState<any>(null);
@@ -17,46 +15,16 @@ export default function Leafletmap() {
     const countryName = feature.properties.name;
 
     if (countryName === 'Portugal') {
-      color = 'blue';
+      color = '#006def';
     }
 
     return {
       fillColor: color,
-      weight: 0.5,
+      weight: 0.9,
       opacity: 1,
       color: '#808080',
-      fillOpacity: 0.6,
+      fillOpacity: 0.3,
     };
-  }
-
-  function highlightFeature(e: L.LeafletMouseEvent) {
-    const layer = e.target;
-    const countryName = layer.feature.properties.name;
-
-    if (layer instanceof L.Path &&  countryName === 'Portugal') {
-      layer.bindPopup(countryName, { closeButton: false }).openPopup();
-      layer.bringToFront();
-
-      layer.on('mousemove', (event) => {
-        const originalLatLng = event.latlng;
-        const newLatLng = L.latLng(originalLatLng.lat + 0.9, originalLatLng.lng);
-        layer.getPopup()?.setLatLng(newLatLng);
-      });
-    }
-  }
-
-  function resetHighlight(e: L.LeafletMouseEvent) {
-    const layer = e.target;
-    if (layer instanceof L.Path) {
-      layer.closePopup();
-    }
-  }
-
-  function handleEachFeature(feature: any, layer: L.Layer) {
-    layer.on({
-      mouseover: highlightFeature,
-      mouseout: resetHighlight,
-    });
   }
 
   return (
@@ -69,13 +37,12 @@ export default function Leafletmap() {
       doubleClickZoom={false}
       zoomControl={false}
       attributionControl={false}
-      style={{ height: '100vh', width: '100%', backgroundColor: '#ffffff' }}
+      style={{ height: '90vh', width: '100%'}}
     >
       {geoData && (
         <GeoJSON
           data={geoData}
           style={countryStyle}
-          onEachFeature={handleEachFeature}
         />
       )}
     </MapContainer>
